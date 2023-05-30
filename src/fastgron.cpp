69d161e5c674737668b7e3376f27f1add779a3b8
adamritter <58403584+adamritter@users.noreply.github.com>
2023-05-30 11:27:56 +0200

    Try to modify data generation, help text

--- src/fastgron.cpp
+++ src/fastgron.cpp
@@ -1,11 +1,55 @@
 #include "simdjson.h"
 #include <fast_io.h>
+#include <dragonbox/dragonbox_to_chars.h>
+#include <fmt/core.h>
 using namespace simdjson;
 using namespace std;
 
 string out;
 const string strue = "true";
 const string sfalse = "false";
+
+struct growing_string
+{
+    std::vector<char> data;
+    size_t len = 0;
+    growing_string() : data(1000) {}
+    growing_string(const char *s) : data(s, s + strlen(s)), len(strlen(s)) {}
+    void reserve_extra(size_t extra)
+    {
+        if (len + extra > data.size())
+        {
+            data.resize(len + extra);
+        }
+    }
+    string_view view() const
+    {
+        return string_view(data.data(), len);
+    }
+
+    growing_string &append(string_view s)
+    {
+        reserve_extra(s.size());
+        memcpy(data.data() + len, s.data(), s.size());
+        len += s.size();
+        return *this;
+    }
+    growing_string &append(char c)
+    {
+        reserve_extra(1);
+        data[len++] = c;
+        return *this;
+    }
+    growing_string &erase(size_t newlen)
+    {
+        len = newlen;
+        return *this;
+    }
+    size_t size() const
+    {
+        return len;
+    }
+};
 
 bool is_js_identifier(string_view s)
 {
@@ -58,13 +102,13 @@ inline int raw_json_string_length(const ondemand::raw_json_string &str)
     }
 }
 
-void recursive_print_gron(ondemand::value element, string &path)
+void recursive_print_gron(ondemand::value element, growing_string &path)
 {
     switch (element.type())
     {
     case ondemand::json_type::array:
     {
-        fast_io::io::print(path, " = [];\n");
+        fast_io::io::print(path.view(), " = [];\n");
         uint64_t index = 0;
         size_t orig_base_len = path.size();
         path.append("[");
@@ -83,7 +127,7 @@ void recursive_print_gron(ondemand::value element, string &path)
     }
     case ondemand::json_type::object:
     {
-        fast_io::io::print(path, " = {};\n");
+        fast_io::io::print(path.view(), " = {};\n");
 
         size_t base_len = path.size();
         for (auto field : element.get_object())
@@ -107,12 +151,42 @@ void recursive_print_gron(ondemand::value element, string &path)
     }
     case ondemand::json_type::number:
     {
-        fast_io::io::print(path, " = ", element.get_double().value(), ";\n");
+        bool use_dragonbox = false;
+        if (use_dragonbox)
+        {
+
+            int base_len = path.size();
+            path.reserve_extra(jkj::dragonbox::max_output_string_length<jkj::dragonbox::ieee754_binary64> + 10);
+            char *ptr = &path.data[base_len];
+            *ptr++ = ' ';
+            *ptr++ = '=';
+            *ptr++ = ' ';
+            double value = element.get_double().value();
+            if (value == 0.0 || value == -0.0)
+            {
+                *ptr++ = '0';
+            }
+            else
+            {
+                // ptr = jkj::dragonbox::to_chars_n(element.get_double().value(), ptr);
+                ptr = fmt::format_to(ptr, "{}", element.get_double().value());
+                // fast_io::io::print(&path.data[base_len], element.get_double().value());
+            }
+            *ptr++ = ';';
+            *ptr++ = '\n';
+            path.len = ptr - &path.data[0];
+            fast_io::io::print(path.view());
+            path.erase(base_len);
+        }
+        else
+        {
+            fast_io::io::print(path.view(), " = ", element.get_double().value(), ";\n");
+        }
         break;
     }
     case ondemand::json_type::string:
     {
-        fast_io::io::print(path, " = \"",
+        fast_io::io::print(path.view(), " = \"",
                            string_view(element.get_raw_json_string().raw(),
                                        raw_json_string_length(element.get_raw_json_string())),
                            "\";\n");
@@ -121,7 +195,7 @@ void recursive_print_gron(ondemand::value element, string &path)
     case ondemand::json_type::boolean:
     {
         fast_io::io::print(
-            path, " = ",
+            path.view(), " = ",
             element.get_bool() ? strue : sfalse, ";\n");
         break;
     }
@@ -129,7 +203,7 @@ void recursive_print_gron(ondemand::value element, string &path)
     {
         if (element.is_null())
         {
-            fast_io::io::print(path, " = null;\n");
+            fast_io::io::print(path.view(), " = null;\n");
         }
         break;
     }
@@ -142,27 +216,56 @@ struct options
 {
     std::string filename;
     bool stream;
+    bool help;
+    bool version;
 };
 
 options parse_options(int argc, char *argv[])
 {
     options opts;
     opts.stream = false;
+    opts.help = false;
+    opts.version = false;
 
-    // Check all arguments
     for (int i = 1; i < argc; ++i)
     {
         if (strcmp(argv[i], "--stream") == 0 || strcmp(argv[i], "-s") == 0)
         {
             opts.stream = true;
         }
+        else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0)
+        {
+            opts.help = true;
+            break; // No need to process further arguments
+        }
+        else if (strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-V") == 0)
+        {
+            opts.version = true;
+            break; // No need to process further arguments
+        }
         else
         {
-            opts.filename = argv[i]; // Assume this is the filename
+            opts.filename = argv[i];
         }
     }
 
     return opts;
+}
+
+void print_help()
+{
+    fast_io::io::perr("Usage: fastgron [OPTIONS] [FILE]\n\n"
+                      "positional arguments:\n"
+                      "  FILE           file name (or '-' for standard input)\n\n"
+                      "options:\n"
+                      "  -h, --help     show this help message and exit\n"
+                      "  -V, --version  show version information and exit\n"
+                      "  -s, --stream   enable stream mode\n");
+}
+
+void print_version()
+{
+    fast_io::io::perr("fastgron version 0.2\n");
 }
 
 #include <fcntl.h>
@@ -203,6 +306,19 @@ int main(int argc, char *argv[])
     ondemand::parser parser;
 
     options opts = parse_options(argc, argv);
+
+    if (opts.help)
+    {
+        print_help();
+        return 0;
+    }
+
+    if (opts.version)
+    {
+        print_version();
+        return 0;
+    }
+
     padded_string json;
     // Check if filename is provided
     if (opts.filename.empty() || opts.filename == "-")
@@ -223,7 +339,7 @@ int main(int argc, char *argv[])
         fast_io::io::print("json = [];\n");
         for (auto doc : docs)
         {
-            string path = "json[" + to_string(index++) + "]";
+            growing_string path = growing_string("json[").append(to_string(index++)).append("]");
             recursive_print_gron(doc, path);
         }
     }
@@ -232,7 +348,7 @@ int main(int argc, char *argv[])
     {
         ondemand::document doc = parser.iterate(json);
         ondemand::value val = doc;
-        string path = "json";
+        growing_string path = "json";
         recursive_print_gron(val, path);
     }
 
